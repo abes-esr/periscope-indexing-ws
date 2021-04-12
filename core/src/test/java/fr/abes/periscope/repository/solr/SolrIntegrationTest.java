3ec40c9e9b8968f627acaf3656447cca19997b22
Fabien Rétif <retif@abes.fr>
2021-04-12 16:17:52 +0200

    Test: ajout des environnements des tests unitaires

--- core/src/test/java/fr/abes/periscope/repository/solr/SolrIntegrationTest.java
+++ core/src/test/java/fr/abes/periscope/repository/solr/SolrIntegrationTest.java
@@ -2,6 +2,7 @@ package fr.abes.periscope.repository.solr;
 
 import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import fr.abes.periscope.CoreTestConfiguration;
 import fr.abes.periscope.EnableOnIntegrationTest;
 import fr.abes.periscope.entity.solr.NoticeSolr;
 import fr.abes.periscope.entity.xml.NoticeXml;
@@ -12,7 +13,9 @@ import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.Resource;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -20,7 +23,7 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 @EnableOnIntegrationTest
-@SpringBootTest(classes = { NoticeMapper.class, NoticeStoreService.class})
+@SpringBootTest(classes = {CoreTestConfiguration.class})
 public class SolrIntegrationTest {
 
     @Autowired
@@ -29,15 +32,15 @@ public class SolrIntegrationTest {
     @Autowired
     private NoticeStoreService noticeService;
 
+    @Value("classpath:noticeXml/13282261X.xml")
+    private Resource xmlFile;
+
     @Test
     @DisplayName("ajout d'une notice avec exemplaire")
     @Disabled
     public void addNoticeToSolr() throws IOException {
 
-        //File file = new File("/work/developpement/projects/periscope/documents/base_xml/13282261X.xml");
-        File file = new File("/work/developpement/projects/periscope/documents/base_xml/039224260.xml");
-        //File file = new File("/work/developpement/projects/periscope/documents/base_xml/000000469.xml");
-        String xml = IOUtils.toString(new FileInputStream(file), StandardCharsets.UTF_8);
+        String xml = IOUtils.toString(new FileInputStream(xmlFile.getFile()), StandardCharsets.UTF_8);
 
         JacksonXmlModule module = new JacksonXmlModule();
         module.setDefaultUseWrapper(false);
@@ -56,9 +59,7 @@ public class SolrIntegrationTest {
     @Disabled
     public void deleteNoticeToSolr() throws IOException {
 
-        File file = new File("/work/developpement/projects/periscope/documents/base_xml/13282261X.xml");
-        //File file = new File("/work/developpement/projects/periscope/documents/base_xml/000000469.xml");
-        String xml = IOUtils.toString(new FileInputStream(file), StandardCharsets.UTF_8);
+        String xml = IOUtils.toString(new FileInputStream(xmlFile.getFile()), StandardCharsets.UTF_8);
 
         JacksonXmlModule module = new JacksonXmlModule();
         module.setDefaultUseWrapper(false);
--- core/src/test/java/fr/abes/periscope/service/NoticesBibioServiceTest.java
+++ core/src/test/java/fr/abes/periscope/service/NoticesBibioServiceTest.java
@@ -1,17 +1,15 @@
 package fr.abes.periscope.service;
 
+import fr.abes.periscope.CoreTestConfiguration;
+import fr.abes.periscope.EnableOnIntegrationTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@ExtendWith(SpringExtension.class)
-@ComponentScan("fr.abes.periscope")
-@SpringBootTest(classes = {NoticesBibioService.class})
+@EnableOnIntegrationTest
+@SpringBootTest(classes = {CoreTestConfiguration.class})
 public class NoticesBibioServiceTest {
 
     @Autowired
